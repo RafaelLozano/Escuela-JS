8947fc3ab4c3464e320bf03e62a3fd71d3c1b115
Rafa <rafa.lozano.rolon@gmail.com>
2020-01-16 19:18:43 -0600

    Herencia prototipal en lugar de clases gracias a ECMAScript 2015

--- Clases/Herencia_prototipal.js
+++ Clases/Herencia_prototipal.js
@@ -1,12 +1,22 @@
+/* En realidad no son clases son prototipos y no existe herencia como tal en JS  */
 function Persona(nombre, apellido, altura) {
     this.name = nombre
     this.lastName = apellido
     this.heigh = altura
 }
+
+function Desarrollador(nombre, apellido) {
+    this.name = nombre
+    this.lastName = apellido
+}
+func
 //prototype
 /* Objeto.prototype.FunctionName=function(Parameter1,Parameter2) */
 Persona.prototype.hello = function() {
     console.log(`Hola me llamo ${this.name}.`)
+}
+Desarrollador.prototype.hello = function() {
+    console.log(`Hola me llamo ${this.name} y soy desarrollador.`)
 }
 var Rafa = new Persona('Rafa', 'Lozano', 1.63)
 Rafa.hello();
